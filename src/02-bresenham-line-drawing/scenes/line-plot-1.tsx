172dc2c7f9f39ae0d0196aa094510ae86ff6f07d
Degreat <mail@degreat.co.uk>
2023-08-13 11:10:37 +0000

    Line plot 1

--- src/02-bresenham-line-drawing/scenes/line-plot-1.tsx
+++ src/02-bresenham-line-drawing/scenes/line-plot-1.tsx
@@ -1,5 +1,5 @@
 import { makeScene2D } from '@motion-canvas/2d'
-import { Vector2, createRef } from '@motion-canvas/core'
+import { Vector2, all, createRef, waitFor, waitUntil } from '@motion-canvas/core'
 import Screen from '../../components/screen'
 
 export default makeScene2D(function* (view) {
@@ -9,12 +9,38 @@ export default makeScene2D(function* (view) {
   const screen = createRef<Screen>()
   view.add(
     <Screen
-      dimension={new Vector2(20, 16)}
+      dimension={new Vector2(12, 12)}
       labelSize={12}
-      pixelSize={20}
+      pixelSize={30}
       ref={screen}
     />
   )
-  screen().originOffset(new Vector2(-5, 3))
-  screen().line(new Vector2(0, 0), new Vector2(7, 7), '#999')
+  screen().originOffset(new Vector2(-3, 2))
+
+  yield* screen().enter()
+
+  yield* waitFor(0.5)
+
+  const start = screen().plot(2, 3, '#999')
+
+  yield* waitUntil('saysixseven')
+
+  const end = screen().plot(6, 7, '#999')
+
+  yield* waitUntil('startplotting')
+
+  yield* all(end().opacity(0.3, 1), start().opacity(0.3, 1))
+
+  yield* waitFor(1)
+
+  const stops = [5.5, 7.5, 2, 2]
+
+  yield* screen().line(
+    new Vector2(2, 3),
+    new Vector2(6, 7),
+    '#999',
+    (i) => stops[i] || stops[0]
+  )
+
+  yield* waitUntil('end')
 })
--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -10,8 +10,10 @@ import {
   Reference,
   SimpleSignal,
   Vector2,
+  all,
   createRef,
   createSignal,
+  waitFor,
 } from '@motion-canvas/core'
 
 interface ScreenProps extends NodeProps {
@@ -28,11 +30,19 @@ interface ScreenProps extends NodeProps {
 class Screen extends Node {
   dimension: Vector2
   private containerRef = createRef<Rect>()
+  private grid = createRef<Grid>()
+  private axes = createRef<Node>()
   labelSize: number
   pixelSize: SimpleSignal<number, void>
   originOffset: SimpleSignal<Vector2, void>
 
-  constructor({ dimension, labelSize = 20, originOffset, pixelSize, ...props }: ScreenProps) {
+  constructor({
+    dimension,
+    labelSize = 20,
+    originOffset,
+    pixelSize,
+    ...props
+  }: ScreenProps) {
     super(props)
     this.dimension = dimension
     this.pixelSize = createSignal(pixelSize)
@@ -46,12 +56,26 @@ class Screen extends Node {
           width={() => this.pixelSize() * this.dimension.x}
           height={() => this.pixelSize() * this.dimension.y}
           spacing={this.pixelSize()}
+          start={0}
+          end={0}
+          ref={this.grid}
         />
 
-        {this.getAxisMarks('x')}
-        {this.getAxisMarks('y')}
+        <Node ref={this.axes} opacity={0}>
+          {this.getAxisMarks('x')}
+          {this.getAxisMarks('y')}
+        </Node>
       </Rect>
     )
+  }
+
+  *enter() {
+    yield* all(
+      this.grid().end(0.5, 1).to(1, 1),
+      this.grid().start(0.5, 1).to(0, 1)
+    )
+
+    yield* this.axes().opacity(1, 0.5)
   }
 
   plot(
@@ -77,7 +101,12 @@ class Screen extends Node {
     return ref
   }
 
-  line(start: Vector2, end: Vector2, fill: PossibleCanvasStyle) {
+  *line(
+    start: Vector2,
+    end: Vector2,
+    fill: PossibleCanvasStyle,
+    wait: (i: number) => number
+  ) {
     const dy = end.y - start.y
     const dx = end.x - start.x
     const m = dy / dx
@@ -86,8 +115,13 @@ class Screen extends Node {
     let x = start.x
     let y = start.y
 
+    let i = 0
+
     while (x <= end.x) {
       this.plot(x, y, fill)
+
+      yield* waitFor(wait(i++))
+
       x++
       if (e >= 0) {
         y++
@@ -140,7 +174,9 @@ class Screen extends Node {
 
           const p = [
             this.pixelSize() / 2 + xOffset,
-            this.containerRef().top().y - height / 2 + this.pixelSize() * 1/4 ,
+            this.containerRef().top().y -
+              height / 2 +
+              (this.pixelSize() * 1) / 4,
           ] as [number, number]
 
           return p
@@ -154,7 +190,9 @@ class Screen extends Node {
             height={this.pixelSize()}
             textAlign={'center'}
             text={() => {
-              const label = `${this.dimension.y / 2 - i + this.originOffset().y}`
+              const label = `${
+                this.dimension.y / 2 - i + this.originOffset().y
+              }`
               return label === '-1' ? '' : label
             }}
           />
@@ -163,7 +201,5 @@ class Screen extends Node {
     )
   }
 }
-
-
 
 export default Screen
